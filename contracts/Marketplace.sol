0290019893de458fcf373f8c8900d7591efe85b4
unknown <shraddha.bhuju@gmail.com>
2024-08-22 11:07:05 +0545

    first commit

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,508 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+import {IMarketplace} from "./interfaces/IMarketplace.sol";
+import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC2771ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
+import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "./extensions/CurrencyTransferLib.sol";
+
+
+contract Marketplace is
+    IMarketplace,
+    AccessControlEnumerableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    // mappings from uid of listing =>listing Info
+    mapping(uint256 => Listing) public listings;
+    uint64 public constant MAX_BPS = 10_000;
+    /// @dev The address that receives all platform fees from all sales.
+    address private platformFeeRecipient;
+    /// @dev The % of primary sales collected as platform fees.
+    uint64 private platformFeeBps;
+    /// @dev Returns the platform fee recipient and bps.
+
+    /// @dev Total number of listings ever created in the marketplace.
+    uint256 public totalListings;
+
+    /// @dev Only lister role holders can create listings, when listings are restricted by lister address.
+    bytes32 private constant LISTER_ROLE = keccak256("LISTER_ROLE");
+    /// @dev The address of the native token wrapper contract.
+    address private immutable nativeTokenWrapper;
+    /*///////////////////////////////////////////////////////////////
+                                Modifiers
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Checks whether caller is a listing creator.
+    modifier onlyListingCreator(uint256 _listingId) {
+        require(listings[_listingId].tokenOwner == msg.sender, "!OWNER");
+        _;
+    }
+
+    /// @dev Checks whether a listing exists.
+    modifier onlyExistingListing(uint256 _listingId) {
+        require(listings[_listingId].assetContract != address(0), "DNE");
+        _;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                    Constructor + initializer logic
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address _nativeTokenWrapper) initializer {
+        nativeTokenWrapper = _nativeTokenWrapper;
+    }
+
+    /// @dev Initializes the contract, like a constructor.
+    function initialize(
+        address _defaultAdmin,
+        address _platformFeeRecipient,
+        uint256 _platformFeeBps
+    ) external initializer {
+        __ReentrancyGuard_init();
+
+        platformFeeBps = uint64(_platformFeeBps);
+        platformFeeRecipient = _platformFeeRecipient;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
+    }
+
+    /// @dev Lets a token owner list tokens for sale: Direct Listing
+    function createListing(ListingParameters memory _params) external override {
+        // Get values to populate `Listing`.
+        uint256 listingId = totalListings;
+        totalListings += 1;
+
+        address tokenOwner = msg.sender;
+        TokenType tokenTypeOfListing = getTokenType(_params.assetContract);
+        uint256 tokenAmountToList = getSafeQuantity(
+            tokenTypeOfListing,
+            _params.quantityToList
+        );
+
+        require(tokenAmountToList > 0, "QUANTITY");
+        require(
+            hasRole(LISTER_ROLE, address(0)) ||
+                hasRole(LISTER_ROLE, msg.sender),
+            "!LISTER"
+        );
+
+        uint256 startTime = _params.startTime;
+        if (startTime < block.timestamp) {
+            // do not allow listing to start in the past (1 hour buffer)
+            require(block.timestamp - startTime < 1 hours, "ST");
+            startTime = block.timestamp;
+        }
+
+        validateOwnershipAndApproval(
+            tokenOwner,
+            _params.assetContract,
+            _params.tokenId,
+            tokenAmountToList,
+            tokenTypeOfListing
+        );
+
+        Listing memory newListing = Listing({
+            listingId: listingId,
+            tokenOwner: tokenOwner,
+            assetContract: _params.assetContract,
+            tokenId: _params.tokenId,
+            startTime: startTime,
+            quantity: tokenAmountToList,
+            currency: _params.currencyToAccept,
+            buyoutPricePerToken: _params.buyoutPricePerToken,
+            tokenType: tokenTypeOfListing
+        });
+
+        listings[listingId] = newListing;
+
+        emit ListingAdded(
+            listingId,
+            _params.assetContract,
+            tokenOwner,
+            newListing
+        );
+    }
+
+    /// @dev Lets a listing's creator edit the listing's parameters.
+    function updateListing(
+        uint256 _listingId,
+        uint256 _quantityToList,
+        uint256 _buyoutPricePerToken,
+        address _currencyToAccept,
+        uint256 _startTime
+    ) external override onlyListingCreator(_listingId) {
+        Listing memory targetListing = listings[_listingId];
+        uint256 safeNewQuantity = getSafeQuantity(
+            targetListing.tokenType,
+            _quantityToList
+        );
+
+        require(safeNewQuantity != 0, "QUANTITY");
+
+        if (_startTime < block.timestamp) {
+            // do not allow listing to start in the past (1 hour buffer)
+            require(block.timestamp - _startTime < 1 hours, "ST");
+            _startTime = block.timestamp;
+        }
+
+        uint256 newStartTime = _startTime == 0
+            ? targetListing.startTime
+            : _startTime;
+        listings[_listingId] = Listing({
+            listingId: _listingId,
+            tokenOwner: _msgSender(),
+            assetContract: targetListing.assetContract,
+            tokenId: targetListing.tokenId,
+            startTime: newStartTime,
+            quantity: safeNewQuantity,
+            currency: _currencyToAccept,
+            buyoutPricePerToken: _buyoutPricePerToken,
+            tokenType: targetListing.tokenType
+        });
+
+        // Must validate ownership and approval of the new quantity of tokens for direct listing.
+        if (targetListing.quantity != safeNewQuantity) {
+            // Transfer all escrowed tokens back to the lister, to be reflected in the lister's
+            // balance for the upcoming ownership and approval check.
+
+            validateOwnershipAndApproval(
+                targetListing.tokenOwner,
+                targetListing.assetContract,
+                targetListing.tokenId,
+                safeNewQuantity,
+                targetListing.tokenType
+            );
+        }
+
+        emit ListingUpdated(_listingId, targetListing.tokenOwner);
+    }
+
+    /// @dev Lets a direct listing creator cancel their listing.
+    function cancelDirectListing(
+        uint256 _listingId
+    ) external onlyListingCreator(_listingId) {
+        Listing memory targetListing = listings[_listingId];
+
+        delete listings[_listingId];
+
+        emit ListingRemoved(_listingId, targetListing.tokenOwner);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Internal functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Validates that `_tokenOwner` owns and has approved Market to transfer NFTs.
+    function validateOwnershipAndApproval(
+        address _tokenOwner,
+        address _assetContract,
+        uint256 _tokenId,
+        uint256 _quantity,
+        TokenType _tokenType
+    ) internal view {
+        address market = address(this);
+        bool isValid;
+
+        if (_tokenType == TokenType.ERC1155) {
+            isValid =
+                IERC1155(_assetContract).balanceOf(_tokenOwner, _tokenId) >=
+                _quantity &&
+                IERC1155(_assetContract).isApprovedForAll(_tokenOwner, market);
+        } else if (_tokenType == TokenType.ERC721) {
+            isValid =
+                IERC721(_assetContract).ownerOf(_tokenId) == _tokenOwner &&
+                (IERC721(_assetContract).getApproved(_tokenId) == market ||
+                    IERC721(_assetContract).isApprovedForAll(
+                        _tokenOwner,
+                        market
+                    ));
+        } else if (_tokenType == TokenType.ERC20) {
+            isValid =
+                IERC1155(_assetContract).balanceOf(_tokenOwner, _tokenId) >=
+                _quantity &&
+                IERC1155(_assetContract).isApprovedForAll(_tokenOwner, market);
+        }
+
+        require(isValid, "!BALNFT");
+    }
+
+    /// @dev Lets an account buy a given quantity of tokens from a listing.
+    function buy(
+        uint256 _listingId,
+        address _buyFor,
+        uint256 _quantityToBuy,
+        address _currency,
+        uint256 _totalPrice
+    ) external payable override nonReentrant onlyExistingListing(_listingId) {
+        Listing memory targetListing = listings[_listingId];
+        address payer = msg.sender;
+
+        // Check whether the settled total price and currency to use are correct.
+        require(
+            _currency == targetListing.currency &&
+                _totalPrice ==
+                (targetListing.buyoutPricePerToken * _quantityToBuy),
+            "!PRICE"
+        );
+
+        executeSale(
+            targetListing,
+            payer,
+            _buyFor,
+            targetListing.currency,
+            targetListing.buyoutPricePerToken * _quantityToBuy,
+            _quantityToBuy
+        );
+    }
+
+    function executeSale(
+        Listing memory _targetListing,
+        address _payer,
+        address _receiver,
+        address _currency,
+        uint256 _currencyAmountToTransfer,
+        uint256 _listingTokenAmountToTransfer
+    ) internal {
+        validateDirectListingSale(
+            _targetListing,
+            _payer,
+            _listingTokenAmountToTransfer,
+            _currency,
+            _currencyAmountToTransfer
+        );
+
+        _targetListing.quantity -= _listingTokenAmountToTransfer;
+        listings[_targetListing.listingId] = _targetListing;
+
+        payout(
+            _payer,
+            _targetListing.tokenOwner,
+            _currency,
+            _currencyAmountToTransfer,
+            _targetListing
+        );
+        transferListingTokens(
+            _targetListing.tokenOwner,
+            _receiver,
+            _listingTokenAmountToTransfer,
+            _targetListing
+        );
+
+        emit NewSale(
+            _targetListing.listingId,
+            _targetListing.assetContract,
+            _targetListing.tokenOwner,
+            _receiver,
+            _listingTokenAmountToTransfer,
+            _currencyAmountToTransfer
+        );
+    }
+
+    function transferListingTokens(
+        address _from,
+        address _to,
+        uint256 _quantity,
+        Listing memory _listing
+    ) internal {
+        if (_listing.tokenType == TokenType.ERC1155) {
+            IERC1155(_listing.assetContract).safeTransferFrom(
+                _from,
+                _to,
+                _listing.tokenId,
+                _quantity,
+                ""
+            );
+        } else if (_listing.tokenType == TokenType.ERC721) {
+            IERC721(_listing.assetContract).safeTransferFrom(
+                _from,
+                _to,
+                _listing.tokenId,
+                ""
+            );
+        }
+    }
+
+    /// @dev Pays out stakeholders in a sale.
+    function payout(
+        address _payer,
+        address _payee,
+        address _currencyToUse,
+        uint256 _totalPayoutAmount,
+        Listing memory _listing
+    ) internal {
+        uint256 platformFeeCut = (_totalPayoutAmount * platformFeeBps) /
+            MAX_BPS;
+
+        uint256 royaltyCut;
+        address royaltyRecipient;
+
+        // Distribute royalties. See Sushiswap's https://github.com/sushiswap/shoyu/blob/master/contracts/base/BaseExchange.sol#L296
+        try
+            IERC2981(_listing.assetContract).royaltyInfo(
+                _listing.tokenId,
+                _totalPayoutAmount
+            )
+        returns (address royaltyFeeRecipient, uint256 royaltyFeeAmount) {
+            if (royaltyFeeRecipient != address(0) && royaltyFeeAmount > 0) {
+                require(
+                    royaltyFeeAmount + platformFeeCut <= _totalPayoutAmount,
+                    "fees exceed the price"
+                );
+                royaltyRecipient = royaltyFeeRecipient;
+                royaltyCut = royaltyFeeAmount;
+            }
+        } catch {}
+
+        // Distribute price to token owner
+        address _nativeTokenWrapper = nativeTokenWrapper;
+
+        CurrencyTransferLib.transferCurrencyWithWrapper(
+            _currencyToUse,
+            _payer,
+            platformFeeRecipient,
+            platformFeeCut,
+            _nativeTokenWrapper
+        );
+        CurrencyTransferLib.transferCurrencyWithWrapper(
+            _currencyToUse,
+            _payer,
+            royaltyRecipient,
+            royaltyCut,
+            _nativeTokenWrapper
+        );
+        CurrencyTransferLib.transferCurrencyWithWrapper(
+            _currencyToUse,
+            _payer,
+            _payee,
+            _totalPayoutAmount - (platformFeeCut + royaltyCut),
+            _nativeTokenWrapper
+        );
+    }
+
+    /// @dev Validates conditions of a direct listing sale.
+    function validateDirectListingSale(
+        Listing memory _listing,
+        address _payer,
+        uint256 _quantityToBuy,
+        address _currency,
+        uint256 settledTotalPrice
+    ) internal {
+        // Check whether a valid quantity of listed tokens is being bought.
+        require(
+            _listing.quantity > 0 &&
+                _quantityToBuy > 0 &&
+                _quantityToBuy <= _listing.quantity,
+            "invalid amount of tokens."
+        );
+
+        // Check if sale is made within the listing window.
+        require(
+           
+                block.timestamp > _listing.startTime,
+            "not within sale window."
+        );
+
+        // Check: buyer owns and has approved sufficient currency for sale.
+        if (_currency == CurrencyTransferLib.NATIVE_TOKEN) {
+            require(msg.value == settledTotalPrice, "msg.value != price");
+        } else {
+            validateERC20BalAndAllowance(_payer, _currency, settledTotalPrice);
+        }
+
+        // Check whether token owner owns and has approved `quantityToBuy` amount of listing tokens from the listing.
+        validateOwnershipAndApproval(
+            _listing.tokenOwner,
+            _listing.assetContract,
+            _listing.tokenId,
+            _quantityToBuy,
+            _listing.tokenType
+        );
+    }
+
+    /// @dev Validates that `_addrToCheck` owns and has approved markeplace to transfer the appropriate amount of currency
+    function validateERC20BalAndAllowance(
+        address _addrToCheck,
+        address _currency,
+        uint256 _currencyAmountToCheckAgainst
+    ) internal view {
+        require(
+            IERC20(_currency).balanceOf(_addrToCheck) >=
+                _currencyAmountToCheckAgainst &&
+                IERC20(_currency).allowance(_addrToCheck, address(this)) >=
+                _currencyAmountToCheckAgainst,
+            "!BAL20"
+        );
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Getter functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Enforces quantity == 1 if tokenType is TokenType.ERC721.
+    function getSafeQuantity(
+        TokenType _tokenType,
+        uint256 _quantityToCheck
+    ) internal pure returns (uint256 safeQuantity) {
+        if (_quantityToCheck == 0) {
+            safeQuantity = 0;
+        } else {
+            safeQuantity = _tokenType == TokenType.ERC721
+                ? 1
+                : _quantityToCheck;
+        }
+    }
+
+    /// @dev Returns the interface supported by a contract.
+    function getTokenType(
+        address _assetContract
+    ) internal view returns (TokenType tokenType) {
+        if (
+            IERC165(_assetContract).supportsInterface(
+                type(IERC1155).interfaceId
+            )
+        ) {
+            tokenType = TokenType.ERC1155;
+        } else if (
+            IERC165(_assetContract).supportsInterface(type(IERC721).interfaceId)
+        ) {
+            tokenType = TokenType.ERC721;
+        } else if (
+            IERC165(_assetContract).supportsInterface(type(IERC20).interfaceId)
+        ) {
+            tokenType = TokenType.ERC20;
+        } else {
+            revert("token must be ERC1155 or ERC721.");
+        }
+    }
+
+    /// @dev Returns the platform fee recipient and bps.
+    function getPlatformFeeInfo() external view returns (address, uint16) {
+        return (platformFeeRecipient, uint16(platformFeeBps));
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Setter functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Lets a contract admin update platform fee recipient and bps.
+    function setPlatformFeeInfo(
+        address _platformFeeRecipient,
+        uint256 _platformFeeBps
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_platformFeeBps <= MAX_BPS, "bps <= 10000.");
+
+        platformFeeBps = uint64(_platformFeeBps);
+        platformFeeRecipient = _platformFeeRecipient;
+
+        emit PlatformFeeInfoUpdated(_platformFeeRecipient, _platformFeeBps);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            Miscellaneous
+    //////////////////////////////////////////////////////////////*/
+}
---
+++ contracts/interfaces/IMarketplace.sol
@@ -0,0 +1,180 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+import "./IPlatformFee.sol";
+interface IMarketplace is IPlatformFee {
+    enum TokenType {
+        ERC20,
+        ERC721,
+        ERC1155
+    }
+
+    /**
+     *  @dev For use in `createListing` as a parameter type.
+     *
+     *  @param assetContract         The contract address of the NFT to list for sale.
+
+     *  @param tokenId               The tokenId on `assetContract` of the NFT to list for sale.
+
+     *  @param startTime             The unix timestamp after which the listing is active. For direct listings:
+     *                               'active' means NFTs can be bought from the listing. For auctions,
+     *                               'active' means bids can be made in the auction.
+     *
+     *  @param secondsUntilEndTime   No. of seconds after `startTime`, after which the listing is inactive.
+     *                               For direct listings: 'inactive' means NFTs cannot be bought from the listing.
+     *                               For auctions: 'inactive' means bids can no longer be made in the auction.
+     *
+     *  @param quantityToList        The quantity of NFT of ID `tokenId` on the given `assetContract` to list. For
+     *                               ERC 721 tokens to list for sale, the contract strictly defaults this to `1`,
+     *                               Regardless of the value of `quantityToList` passed.
+     *
+     *  @param currencyToAccept      For direct listings: the currency in which a buyer must pay the listing's fixed price
+     *                               to buy the NFT(s). For auctions: the currency in which the bidders must make bids.
+     *
+     *  @param reservePricePerToken  For direct listings: this value is ignored. For auctions: the minimum bid amount of
+     *                               the auction is `reservePricePerToken * quantityToList`
+     *
+     *  @param buyoutPricePerToken   For direct listings: interpreted as 'price per token' listed. For auctions: if
+     *                               `buyoutPricePerToken` is greater than 0, and a bidder's bid is at least as great as
+     *                               `buyoutPricePerToken * quantityToList`, the bidder wins the auction, and the auction
+     *                               is closed.
+     *
+     *  @param listingType           The type of listing to create - a direct listing or an auction.
+    **/
+    struct ListingParameters {
+        address assetContract;
+        uint256 tokenId;
+        uint256 startTime;
+        uint256 quantityToList;
+        address currencyToAccept;
+        uint256 buyoutPricePerToken;
+    }
+
+
+        /**
+     *  @notice The information related to a listing; either (1) a direct listing, or (2) an auction listing.
+     *
+     *  @dev For direct listings:
+     *          (1) `reservePricePerToken` is ignored.
+     *          (2) `buyoutPricePerToken` is simply interpreted as 'price per token'.
+     *
+     *  @param listingId             The uid for the listing.
+     *
+     *  @param tokenOwner            The owner of the tokens listed for sale.  
+     *
+     *  @param assetContract         The contract address of the NFT to list for sale.
+
+     *  @param tokenId               The tokenId on `assetContract` of the NFT to list for sale.
+
+     *  @param startTime             The unix timestamp after which the listing is active. For direct listings:
+     *                               'active' means NFTs can be bought from the listing. For auctions,
+     *                               'active' means bids can be made in the auction.
+     *
+     *  @param endTime               The timestamp after which the listing is inactive.
+     *                               For direct listings: 'inactive' means NFTs cannot be bought from the listing.
+     *                               For auctions: 'inactive' means bids can no longer be made in the auction.
+     *
+     *  @param quantity              The quantity of NFT of ID `tokenId` on the given `assetContract` listed. For
+     *                               ERC 721 tokens to list for sale, the contract strictly defaults this to `1`,
+     *                               Regardless of the value of `quantityToList` passed.
+     *
+     *  @param currency              For direct listings: the currency in which a buyer must pay the listing's fixed price
+     *                               to buy the NFT(s). For auctions: the currency in which the bidders must make bids.
+     *
+     *  @param reservePricePerToken  For direct listings: this value is ignored. For auctions: the minimum bid amount of
+     *                               the auction is `reservePricePerToken * quantityToList`
+     *
+     *  @param buyoutPricePerToken   For direct listings: interpreted as 'price per token' listed. For auctions: if
+     *                               `buyoutPricePerToken` is greater than 0, and a bidder's bid is at least as great as
+     *                               `buyoutPricePerToken * quantityToList`, the bidder wins the auction, and the auction
+     *                               is closed.
+     *
+     *  @param tokenType             The type of the token(s) listed for for sale -- ERC721 or ERC1155 
+     *
+     * @param listingType            The type of listing to create - a direct listing or an auction.
+    **/
+
+    struct Listing {
+        uint256 listingId;
+        address tokenOwner;
+        address assetContract;
+        uint256 tokenId;
+        uint256 startTime;
+        uint256 quantity;
+        address currency;
+        uint256 buyoutPricePerToken;
+        TokenType tokenType;
+    }
+
+    /// @dev Emitted when a new listing is created.
+    event ListingAdded(
+        uint256 indexed listingId,
+        address indexed assetContract,
+        address indexed lister,
+        Listing listing
+    );
+
+    /// @dev Emitted when the parameters of a listing are updated.
+    event ListingUpdated(
+        uint256 indexed listingId,
+        address indexed listingCreator
+    );
+
+    /// @dev Emitted when a listing is cancelled.
+    event ListingRemoved(
+        uint256 indexed listingId,
+        address indexed listingCreator
+    );
+
+      /**
+     * @dev Emitted when a buyer buys from a direct listing, or a lister accepts some
+     *      buyer's offer to their direct listing.
+     */
+    event NewSale(
+        uint256 indexed listingId,
+        address indexed assetContract,
+        address indexed lister,
+        address buyer,
+        uint256 quantityBought,
+        uint256 totalPricePaid
+    );
+
+
+    function createListing(ListingParameters memory _params) external;
+ function updateListing(
+        uint256 _listingId,
+        uint256 _quantityToList,
+        uint256 _buyoutPricePerToken,
+        address _currencyToAccept,
+        uint256 _startTime
+    ) external;
+
+    /**
+     *  @notice Lets a direct listing creator cancel their listing.
+     *
+     *  @param _listingId The unique Id of the listing to cancel.
+     */
+    function cancelDirectListing(uint256 _listingId) external;
+   /**
+     *  @notice Lets someone buy a given quantity of tokens from a direct listing by paying the fixed price.
+     *
+     *  @param _listingId The uid of the direct listing to buy from.
+     *  @param _buyFor The receiver of the NFT being bought.
+     *  @param _quantity The amount of NFTs to buy from the direct listing.
+     *  @param _currency The currency to pay the price in.
+     *  @param _totalPrice The total price to pay for the tokens being bought.
+     *
+     *  @dev A sale will fail to execute if either:
+     *          (1) buyer does not own or has not approved Marketplace to transfer the appropriate
+     *              amount of currency (or hasn't sent the appropriate amount of native tokens)
+     *
+     *          (2) the lister does not own or has removed Marketplace's
+     *              approval to transfer the tokens listed for sale.
+     */
+    function buy(
+        uint256 _listingId,
+        address _buyFor,
+        uint256 _quantity,
+        address _currency,
+        uint256 _totalPrice
+    ) external payable;
+}
